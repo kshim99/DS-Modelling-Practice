f79541d1843d4fcbbdd3714332973f19b6df0957
Kyu Min Shim <kmshim@uwaterloo.ca>
2024-07-17 16:37:23 -0400

    Initializing Classification Example

---
+++ Classification_example.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import altair as alt
+
+cancer = pd.read_csv("data/wdbc.data")
+
+#names = ["ID","Class","Radius","Texture","Perimeter","Area","Smoothness","Compactness","Concavity","Concave_Points","Symmetry","Fractal_Dimension"])
